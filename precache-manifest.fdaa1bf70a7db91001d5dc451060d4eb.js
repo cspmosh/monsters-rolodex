b124b81b21018a707a650f41d1f4f503d491d828
Josh Turner <cs.pmosh@gmail.com>
2020-01-16 15:07:55 -0600

    Updates

---
+++ precache-manifest.fdaa1bf70a7db91001d5dc451060d4eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8e890536789108c0da771ded8311776",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d0018defc9a15a05f568",
+    "url": "/monsters-rolodex/static/css/main.ba6fc300.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0018defc9a15a05f568",
+    "url": "/monsters-rolodex/static/js/main.7cfdaf2f.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
